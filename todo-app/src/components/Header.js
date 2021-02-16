6716f208bc03f8634bb6cb8d78b96891d00d1002
emilyzhang <emily@hesper.io>
2021-02-16 13:33:58 -0500

    publish

---
+++ todo-app/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+
+    return (
+        <div className="header">
+            <h1>Azure Static Web Apps With React Demo</h1>
+        </div>
+    )
+}
+
+export default Header
