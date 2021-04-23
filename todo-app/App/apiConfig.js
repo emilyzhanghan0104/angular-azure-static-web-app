4792cac6cea4a09de498e3d7a231a331919348d5
emilyzhang <emily@hesper.io>
2021-04-22 21:48:24 -0400

    first commit

--- todo-app/App/apiConfig.js
+++
@@ -1,5 +0,0 @@
-// The current application coordinates were pre-registered in a B2C tenant.
-const apiConfig = {
-  b2cScopes: ["https://hesperTest.onmicrosoft.com/api/demo.read"],
-  webApi: "http://localhost:5000/hello",
-};
